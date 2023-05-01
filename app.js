40b7fe75cfe3a9a5c59bd1c4f68b4a4bae962d5a
alefra88 <efra.rlz@gmail.com>
2023-05-01 02:44:23 -0600

    middleware and statics files

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const express = require('express');
+const express = require("express");
+const morgan = require("morgan");
 
 // express app
 const app = express();
@@ -7,27 +8,59 @@ const app = express();
 app.listen(3000);
 
 // register view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 // app.set('views', 'myviews');
 
-app.get('/', (req, res) => {
+// middleware & static files
+app.use(express.static("public"));
+
+// app.use((req, res, next) => {
+//   console.log("new request made:");
+//   console.log("host: ", req.hostname);
+//   console.log("path: ", req.path);
+//   console.log("method: ", req.method);
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   console.log("in the next middleware");
+//   next();
+// });
+
+app.use(morgan("dev"));
+
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
+
+app.get("/", (req, res) => {
   const blogs = [
-    {title: 'Nodejs test title', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Nodejs and ejs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Nodejs for Dummies', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {
+      title: "Nodejs test title",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Nodejs and ejs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Nodejs for Dummies",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
   ];
-  res.render('index', { title: 'Home', blogs });
+  res.render("index", { title: "Home", blogs });
 });
 
-app.get('/about', (req, res) => {
-  res.render('about', { title: 'About' });
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
 });
 
-app.get('/blogs/create', (req, res) => {
-  res.render('create', { title: 'Create a new blog' });
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new blog" });
 });
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).render('404', { title: '404' });
+  res.status(404).render("404", { title: "404" });
 });
