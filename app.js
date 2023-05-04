ade1a8c20f55363825db3433677c4027b1349840
alefra88 <efra.rlz@gmail.com>
2023-05-04 04:52:26 -0600

    POST made

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ app.set("view engine", "ejs");
 
 // middleware & static files
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
 //Testing
 //Add blogs
@@ -84,6 +86,19 @@ app.get("/blogs", (req, res) => {
     .sort({ createdAt: -1 })
     .then((result) => {
       res.render("index", { title: "All blogs", blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+//POST
+app.post("/blogs", (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog
+    .save()
+    .then((result) => {
+      res.redirect("/blogs");
     })
     .catch((err) => {
       console.log(err);
