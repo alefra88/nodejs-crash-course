6d2313c14f837b8bca13254f1599aa8a10d22dd2
alefra88 <efra.rlz@gmail.com>
2023-05-04 23:00:12 -0600

    add pathings

--- app.js
+++ app.js
@@ -104,6 +104,19 @@ app.post("/blogs", (req, res) => {
       console.log(err);
     });
 });
+
+//getting single blog
+app.get("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) => {
+      res.render("details", { blog: result, title: "Blog Details" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 // 404 page
 app.use((req, res) => {
   res.status(404).render("404", { title: "404" });
