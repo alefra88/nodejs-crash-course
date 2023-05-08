7988bcec2d3397ef11df3c3bc3b76a64e06e99b4
alefra88 <efra.rlz@gmail.com>
2023-05-08 05:25:24 -0600

    end project

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ mongoose
 
 // register view engine
 app.set("view engine", "ejs");
+
 // app.set('views', 'myviews');
 
 // middleware & static files
@@ -40,43 +41,49 @@ app.get("/add-blog", (req, res) => {
       console.log(err);
     });
 });
+
 //get all blogs
-app.get("/all-blogs", (req, res) => {
-  Blog.find()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-//get a single blog
-app.get("/single-blog", (req, res) => {
-  Blog.findById("645223a283a6c901bd715e0f")
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+// app.get("/all-blogs", (req, res) => {
+//   Blog.find()
+//     .then((result) => {
+//       res.send(result);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
+
+// //get a single blog
+// app.get("/single-blog", (req, res) => {
+//   Blog.findById("645223a283a6c901bd715e0f")
+//     .then((result) => {
+//       res.send(result);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
 //Dev tool
 // app.use(morgan("dev"));
 // app.use((req, res, next) => {
 //   res.locals.path = req.path;
 //   next();
 // });
-//blog routes
+
+
 //HOME
 app.get("/", (req, res) => {
   res.redirect("/blogs");
 });
+
 //ABOUT
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
+
 //BLOG ROUTES
 app.use("/blogs", blogRoutes);
+
 // 404 page
 app.use((req, res) => {
   res.status(404).render("404", { title: "404" });
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,59 +1,20 @@
 const express = require("express");
-const Blog = require("../models/blog");
+const blogController = require("../controllers/blogController");
 const router = express.Router();
 
 //ALL BLOGS
-router.get("/", (req, res) => {
-  Blog.find()
-    .sort({ createdAt: -1 })
-    .then((result) => {
-      res.render("index", { title: "All blogs", blogs: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-//POST
-router.post("/", (req, res) => {
-  const blog = new Blog(req.body);
+router.get("/", blogController.blog_index);
 
-  blog
-    .save()
-    .then((result) => {
-      res.redirect("/blogs");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+//POST
+router.post("/", blogController.blog_create_post);
 
 //create blog
-router.get("/create", (req, res) => {
-  res.render("create", { title: "Create a new blog" });
-});
+router.get("/create", blogController.blog_create_get);
 
-//getting single blog
-router.get("/:id", (req, res) => {
-  const id = req.params.id;
-  Blog.findById(id)
-    .then((result) => {
-      res.render("details", { blog: result, title: "Blog Details" });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+//details
+router.get("/:id", blogController.blog_details);
+
 //DELETE BLOG
-router.delete("/:id", (req, res) => {
-  const id = req.params.id;
-
-  Blog.findByIdAndDelete(id)
-    .then((result) => {
-      res.json({ redirect: "/blogs" });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.delete("/:id", blogController.blog_delete);
 
 module.exports = router;
